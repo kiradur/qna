c267a210077d45e46f5fddc93f15f759edf8ff79
Кирилл Войтулевич <voytulevich.k@gmail.com>
2019-07-18 13:56:17 +0300

    lesson 5

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -2,12 +2,15 @@ require 'rails_helper'
 
 RSpec.describe QuestionsController, type: :controller do
   let(:question) { create(:question) }
+  let(:user) { create(:user) }
+  let(:other_user) { create(:user) }
 
-  describe 'Get #index' do
+  describe 'GET #index' do
     let(:questions) { create_list(:question, 3) }
 
     before { get :index }
-    it 'populates an array of all questions' do
+
+    it 'populats an array of all questions' do
       expect(assigns(:questions)).to match_array(questions)
     end
 
@@ -19,35 +22,42 @@ RSpec.describe QuestionsController, type: :controller do
   describe 'GET #show' do
     before { get :show, params: { id: question } }
 
-    it 'renders show view' do
+    it 'renders show view' do      
       expect(response).to render_template :show
     end
   end
 
   describe 'GET #new' do
+    before { login(user) }
+
     before { get :new }
 
-    it 'assigns a new Question to @question' do
-      expect(assigns(:question)).to be_a_new(Question)
-    end
-
-    it 'renders new view' do
+    it 'renders new view' do      
       expect(response).to render_template :new
     end
   end
 
   describe 'GET #edit' do
+    before { login(user) }
+
     before { get :edit, params: { id: question } }
 
-    it 'renders edit view' do
+    it 'renders edit view' do      
       expect(response).to render_template :edit
     end
   end
 
   describe 'POST #create' do
+    before { login(user) }
+
     context 'with valid attributes' do
-      it 'saves a new question in the database' do
+      it 'saves a new question in the db' do
         expect { post :create, params: { question: attributes_for(:question) } }.to change(Question, :count).by(1)
+      end
+
+      it 'created question belongs to current_user' do
+        post :create, params: { question: attributes_for(:question) }
+        expect(assigns(:question).user).to eq user
       end
 
       it 'redirects to show view' do
@@ -69,6 +79,8 @@ RSpec.describe QuestionsController, type: :controller do
   end
 
   describe 'PATCH #update' do
+    before { login(user) }
+
     context 'with valid attributes' do
 
       it 'assigns the requested question to @question' do
@@ -97,8 +109,8 @@ RSpec.describe QuestionsController, type: :controller do
       it 'does not change question' do
         question.reload
 
-        expect(question.title).to eq 'MyString'
-        expect(question.body).to eq 'MyText'
+        expect(question.title).to eq 'QuestionTitle'
+        expect(question.body).to eq 'QuestionBody'
       end
 
       it 're-renders edit view' do        
@@ -109,18 +121,34 @@ RSpec.describe QuestionsController, type: :controller do
   end
 
   describe 'DELETE #destroy' do
-    let!(:question) { create(:question) }
+    before { login(user) }
+    let!(:question) { create(:question, user: user) }
 
-    it 'deletes the question' do
-      expect { delete :destroy, params: { id: question } }.to change(Question, :count).by(-1)
+    context 'author question' do
+
+      it 'deletes the question' do
+        expect { delete :destroy, params: { id: question } }.to change(Question, :count).by(-1)
+      end
+
+      it 'redirects to index' do
+        delete :destroy, params: { id: question }
+        expect(response).to redirect_to questions_path
+      end
+
     end
 
-    it 'redirects to index' do
-      delete :destroy, params: { id: question }
-      expect(response).to redirect_to questions_path
+    context 'not author question' do
+      before { login(other_user) }
+
+      it 'deletes the question' do
+        expect { delete :destroy, params: { id: question } }.to_not change(Question, :count)
+      end
+
+      it 're-render to question' do
+        delete :destroy, params: { id: question }
+        expect(response).to redirect_to question_path(assigns(:question))
+      end
     end
 
   end
-
-
 end
