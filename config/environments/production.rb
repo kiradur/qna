3517fa18f897f1e3d517432e81effbf911110f8f
Кирилл Войтулевич <voytulevich.k@gmail.com>
2019-09-19 15:43:35 +0300

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,13 +39,12 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
-  config.active_storage.service = :amazon
+  config.active_storage.service = :local
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
-  config.action_cable.allowed_request_origins = ['http://45.67.230.164/']
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
@@ -62,7 +61,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "QnA_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "qna_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
 
@@ -92,9 +91,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-  # config.action_mailer.smtp_settings
-
-
-  config.action_mailer.default_url_options = { host: '157.245.72.235' }
 end
